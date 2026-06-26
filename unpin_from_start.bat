0026869fd2d6f36aff4bf7ca5512d1a67397cdb1
gxy <gotogxy@outlook.com>
2026-06-26 10:24:51 +0800

    fix: move PowerShell logic to ps1 files to avoid bat quoting issues

--- unpin_from_start.bat
+++ unpin_from_start.bat
@@ -1,16 +1,3 @@
 @echo off
-setlocal
-
-set "START_MENU=%APPDATA%\Microsoft\Windows\Start Menu\Programs"
-set "SHORTCUT_PATH=%START_MENU%\App Hotkey Manager.lnk"
-
-if exist "%SHORTCUT_PATH%" (
-  del /f /q "%SHORTCUT_PATH%"
-  echo Shortcut removed.
-) else (
-  echo Shortcut not found.
-)
-
-echo.
-pause
-endlocal
+cd /d "%~dp0"
+powershell -NoProfile -ExecutionPolicy Bypass -File "%~dp0unpin_from_start.ps1"
