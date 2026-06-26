0c73c599c4f07b9e3ca3668d68f32c9f0ae385a1
gxy <gotogxy@outlook.com>
2026-06-26 09:36:20 +0800

    fix: uninstall_startup.bat targets .bat instead of .exe

--- uninstall_startup.bat
+++ uninstall_startup.bat
@@ -1,17 +1,17 @@
 @echo off
 setlocal
 
-set "TARGET_EXE=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\app_hotkey_manager.exe"
+set "TARGET=%APPDATA%\Microsoft\Windows\Start Menu\Programs\Startup\app_hotkey_manager.bat"
 
-if exist "%TARGET_EXE%" (
-  del /f /q "%TARGET_EXE%"
-  if exist "%TARGET_EXE%" (
+if exist "%TARGET%" (
+  del /f /q "%TARGET%"
+  if exist "%TARGET%" (
     echo Remove failed.
   ) else (
     echo Startup entry removed.
   )
 ) else (
-  echo Startup EXE was not found.
+  echo Startup entry was not found.
 )
 
 echo.
